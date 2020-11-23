0284f2b4ddc4d49aedc7b9ba00480f1b842fafbf
Sebastian Karlsen <sebastian@karlsen.fr>
2020-11-23 09:57:19 +0100

    Fix indent

--- main.cpp
+++ main.cpp
@@ -14,17 +14,17 @@ int main() {
 	universe.add_body(new Body(
 		5.0,
 		Point(0.0, 0.0, 0.0), 
-      	Point(0.0, 0.0, 0.0), 
-      	Point(0.0, 0.0, 0.0)
+		Point(0.0, 0.0, 0.0), 
+		Point(0.0, 0.0, 0.0)
     ));
 
 	// Planet 1: mass = 1
 	universe.add_body(new Body(
-        1.0,
-        Point(0.5, 0.5, 0.0),
-        Point(0.0, -0.2, 0.0),
-        Point(0.0, 0.0, 0.0)
-    ));
+		1.0,
+		Point(0.5, 0.5, 0.0),
+		Point(0.0, -0.2, 0.0),
+		Point(0.0, 0.0, 0.0)
+	));
 
 	// run(delta t, t_0 and t_end)
     universe.run(0.001, 0.0, 10.0);
